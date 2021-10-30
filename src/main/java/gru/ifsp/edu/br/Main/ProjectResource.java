b724f9cab536518ebd6de33f47d70775fb423921
Jonatha7 <jonatha7silva@hotmail.com>
2021-10-30 18:26:58 -0300

    Modificado método para buscar o json e setar nas propriedades

--- src/main/java/gru/ifsp/edu/br/Main/ProjectResource.java
+++ src/main/java/gru/ifsp/edu/br/Main/ProjectResource.java
@@ -1,15 +1,14 @@
 package gru.ifsp.edu.br.Main;
 
 import java.util.List;
-
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("projects")
+@RequestMapping("project")
 public class ProjectResource {
 	
 	private final ProjectRepository repository;
@@ -18,16 +17,19 @@ public class ProjectResource {
 		this.repository = repository;
 	}
 	
+	@Autowired
+	private Search search;
+	
 	
 	@GetMapping("/all")
 	public List<Project> project() {
 		return repository.findAll();
 	}
-
-	@GetMapping("/123")
-	public @ResponseBody Project[] findAllObjects() {
-		Project[] projects;
-	    projects = new Search().repositories();
-	    return projects;
+	
+	@GetMapping("/search/{text}")
+	public Project[] Find(@PathVariable("text") String text) throws Exception {
+		Project[] projects = new Project[3];
+		projects = search.doGet(projects, text);
+		return projects;
 	}
 }
--- src/main/java/gru/ifsp/edu/br/Main/Search.java
+++ src/main/java/gru/ifsp/edu/br/Main/Search.java
@@ -1,16 +1,49 @@
 package gru.ifsp.edu.br.Main;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.web.client.RestTemplate;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class Search {
 	
-	public Project[] repositories() {
-		RestTemplate restTemplate = new RestTemplate();
-		RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
-		restTemplate = restTemplateBuilder.build();
+	public Project[] doGet(Project[] projects, String text) throws Exception {
+		String txt = "", response = "";
+		String baseUrl = "https://api.github.com/search/repositories?q=" + text + "&per_page=3&sort:star"; 
+		try {
+			URL url = new URL(baseUrl);
+		    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		    if (connection.getResponseCode() != 200)
+				throw new RuntimeException("HTTP error code : " + connection.getResponseCode());
+			BufferedReader buffereReader = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+			while ((response = buffereReader.readLine()) != null) {
+				txt += response;
+	        }	
+		} catch (Exception e) {
+			throw new Exception("ERRO: " + e);
+		}
 		
-		Project[] responseEntity = restTemplate.getForObject("https://api.github.com/search/repositories?q=laravel", Project[].class);
-		return responseEntity;
+		JSONObject repositories = new JSONObject(txt);
+		JSONArray arrItems = repositories.getJSONArray("items");
+		
+		for(int i = 0; i < arrItems.length(); i++) {
+			JSONObject items = arrItems.getJSONObject(i);
+			JSONObject arrAvatar = items.getJSONObject("owner");
+			
+			projects[i] = new Project();
+			projects[i].setId(items.getInt("id"));
+			projects[i].setName(items.getString("name"));
+			projects[i].setAvatar_url(arrAvatar.getString("avatar_url"));
+			projects[i].setDescription(items.getString("description"));
+			projects[i].setHtml_url(items.getString("html_url"));
+			projects[i].setLanguage(items.getString("language"));
+		}
+		return projects;
 	}
 }

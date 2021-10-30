126ae4a3966389b3130ecaceb4bacf1544da0ce3
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2021-10-30 11:47:43 -0300

    tela de resultados

---
+++ frontend/js/results.js
@@ -0,0 +1,40 @@
+const favoriteBtns = Array.from(document.querySelectorAll('.favorite-btn'))
+favoriteBtns.forEach(btn => {
+    const icon = btn.querySelector('i')
+    let fav = parseInt(btn.dataset.fav)
+
+    if(!fav){
+        icon.classList.remove('fas')
+        icon.classList.add('far')
+    }
+
+    icon.addEventListener('click', e => {
+        e.stopPropagation()
+        btn.click()
+    })
+
+    btn.addEventListener('click', e => {
+        let favBtn = e.target
+        let {rep} = favBtn.dataset
+        let fav = parseInt(favBtn.dataset.fav)
+
+        if(fav){
+            icon.classList.remove('fas')
+            icon.classList.add('far')
+            favBtn.dataset.fav =  '0'
+        }else{
+            icon.classList.add('fas')
+            icon.classList.remove('far')
+            favBtn.dataset.fav =  '1'
+        }
+            
+        $.ajax({
+            url: '', //definir URL do método
+            data: {
+                id: rep, //id do repositório
+                favorite: fav //boolean para saber se foi favoritado (1) ou desfavoritado (0)
+            },
+            type: 'POST'
+        })
+    })
+})
